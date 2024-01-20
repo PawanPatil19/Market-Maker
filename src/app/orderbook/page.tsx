334f2592665b417fdd1b2d3910136422a680af50
PawanPatil19 <pawankp1903@gmail.com>
2024-01-20 18:25:18 +0800

    commit

--- src/app/orderbook/page.tsx
+++ src/app/orderbook/page.tsx
@@ -10,17 +10,29 @@ export default function Home() {
                 <h1 className='text-4xl font-bold py-10 text-white'>Order Book</h1>
             </div>
 
-            <div className='flex justify-center items-center'>
+            <div className="absolute left-1/2 -ml-0.5 w-0.5 h-screen bg-gray-600"></div>
+
+            <div className='flex justify-center items-center pb-10'>
                 <div className='flex flex-row space-x-20'>
                     <h1 className='text-md font-light text-white'>Quantity</h1>
-                    <div>
-                        <h1 className='text-md font-light text-white'>Bid</h1>
-                    </div>
+                    <h1 className='text-md font-light text-white'>Bid</h1>
 
                     <h1 className='text-md font-light text-white'>Ask</h1>
                     <h1 className='text-md font-light text-white'>Quantity</h1>
                 </div>
             </div>
+
+            <div className='flex justify-center items-center'>
+                <div className='flex flex-row space-x-20'>
+                    <h1 className='text-md font-light text-white'>4</h1>
+                    <h1 className='text-md font-light text-green-500'>10000</h1>
+
+                    <h1 className='text-md font-light text-red-500'>20000</h1>
+                    <h1 className='text-md font-light text-white'>2</h1>
+                </div>
+            </div>
+
+
             
         
         </div>
